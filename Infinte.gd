91fb3b8ffd949d82e0511e27a25ca485f81a306b
Priyanshu-1905 <priyanshukapadia195@gmail.com>
2024-02-21 01:02:25 +0530

    ingfff

---
+++ Infinte.gd
@@ -0,0 +1,14 @@
+extends TileMap
+
+var road_tileset = preload("res://Infinte.gd") # Replace with your road tileset path
+var tile_size = Vector2(32, 32) # Change this to match your tile size
+var road_width = 5 # Change this to set the road width
+
+func _ready():
+	create_road(Vector2(0, 0))
+
+func create_road(start_position):
+	for x in range(road_width):
+		for y in range(1000): # Set the desired length of the road
+			var cell_position = start_position + Vector2(x, y) * tile_size
+			add_tile(road_tileset, cell_position)
